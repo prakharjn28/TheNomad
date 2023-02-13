53cd237f26977fc02dac289b65cef86d0326bc2f
Giao U <71329441+giaou@users.noreply.github.com>
2023-02-13 08:22:39 -0800

    deleted floating button
     Floating button got deleted since it is not used anymore

--- homepageview.dart
+++ homepageview.dart
@@ -38,11 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: ()=> context.push('/details?states="Details"'),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
